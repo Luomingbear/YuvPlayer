0f5b7100fa50a8700aaa353d6518b9f31ce10524
Ming Luo <luomingbear@163.com>
2016-06-09 18:07:44 +0800

    新建了一个activity,准备自定义sdl的显示view;opengl的计划搁置,需要使用jni,而opengl比sdl代码更加复杂所以还是使用sdl简洁

--- app/src/main/java/com/bear/yuvplayer/MainActivity.java
+++ app/src/main/java/com/bear/yuvplayer/MainActivity.java
@@ -5,9 +5,11 @@ import android.app.ActivityManager;
 import android.content.Context;
 import android.content.pm.ConfigurationInfo;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by bear on 16-6-6.
@@ -16,11 +18,13 @@ public class MainActivity extends Activity{
 
     private TextView textview;
 
-    private Button   ok;
+    private Button   button_convert,button_play;
 
     private EditText editText_input;
 
-    private EditText editText_output;
+    private String   src;   //地址
+
+    private boolean Play=false,Convert=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,11 +37,41 @@ public class MainActivity extends Activity{
         textview=(TextView)findViewById(R.id.textView);
         textview.setText(isOpenGLES2(getApplicationContext()));
 
-        //
+        //输入视频的地址
+        editText_input=(EditText) findViewById(R.id.text_input);
 
+        //转码
+        button_convert=(Button)findViewById(R.id.button_convert);
+        button_convert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //
+                src = editText_input.getText().toString();
+
+                Toast.makeText(getApplicationContext(),"Convert!",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+        //播放
+        button_play=(Button)findViewById(R.id.button_play);
+        button_play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //
+                src = editText_input.getText().toString();
+
+                Toast.makeText(getApplicationContext(),"Play!",Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
     }
 
+    //
     public String isOpenGLES2(Context context){
         ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
         ConfigurationInfo info = am.getDeviceConfigurationInfo();
---
+++ app/src/main/java/org/libsdl/app/SDLView.java
@@ -0,0 +1,20 @@
+package org.libsdl.app;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+
+/**
+ * Created by bear on 16-6-9.
+ */
+public class SDLView extends SurfaceView {
+    //
+    public SDLView(Context context, AttributeSet attributeSet){
+        super(context,attributeSet);
+
+    }
+
+    //
+
+
+}
